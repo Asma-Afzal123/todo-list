c6eaf97b1ba441ad66217b85955268d123e11630
Asma-Afzal123 <162578850+Asma-Afzal123@users.noreply.github.com>
2024-05-08 00:10:36 +0500

    create file

---
+++ main.ts
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let todos : any[] = []
+while(true){
+    let input = await inquirer.prompt([
+
+{
+    name:"Todoitem",
+    type:"input",
+    message:'add todo item in listing'
+},
+
+{
+    name:"addMore",
+    type:"list",
+    choices: ["yes","no"]
+},
+
+    ]);
+
+
+
+    const{Todoitem,addMore}=input;
+
+    todos.push(Todoitem);
+if (addMore == "no"){
+    console.log("Todolist:");
+    for(let i=0;i<todos.length; i++)
+{console.log(todos[i])}
+        
+break;    
+}
+
+}
